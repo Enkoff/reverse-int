c002ccad4ccd07ae966928172c9fa4e3b0b8d666
Enkoff <0639818413@ukr.net>
2020-10-19 12:00:57 +0300

    fix: change function twelSort

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+        let string = '' + n;
+        let res;
+        (n > 0) ? res = Number(string.split("").reverse().join("")) : res = Number(string.slice(1).split("").reverse().join(""));
+        return res;
 }
